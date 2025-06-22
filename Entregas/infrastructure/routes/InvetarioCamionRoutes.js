a367399a8232b7edd1b3472a116d13948c9890c5
José Miguel Vargas <51755883+Jvargast@users.noreply.github.com>
2025-06-21 22:35:38 -0400

    feat: add camion emptying flow

--- Entregas/infrastructure/routes/InvetarioCamionRoutes.js
+++ Entregas/infrastructure/routes/InvetarioCamionRoutes.js
@@ -11,8 +11,13 @@ router.get("/disponible/chofer", checkPermissions("entregas.inventariocamion.dis
 router.get("/disponible/:id_camion", checkPermissions("entregas.inventariocamion.disponible"),InventarioCamionController.getInventarioDisponible); 
 router.get("/inventario/chofer/:id_chofer", checkPermissions("entregas.inventariocamion.porchofer"), InventarioCamionController.getInventarioPorChofer);
 router.get("/estado/:id_camion", checkPermissions("entregas.inventariocamion.estado"), InventarioCamionController.getEstadoInventarioCamion);
-router.post("/return/:id", checkPermissions(""), InventarioCamionController.returnProducts); 
-router.get("/:id", checkPermissions("entregas.inventariocamion.ver"),InventarioCamionController.getProductsByCamion); 
+router.post("/return/:id", checkPermissions(""), InventarioCamionController.returnProducts);
+router.get("/:id", checkPermissions("entregas.inventariocamion.ver"),InventarioCamionController.getProductsByCamion);
+router.post(
+  "/vaciar/:id_camion",
+  checkPermissions("entregas.inventariocamion.vaciar"),
+  InventarioCamionController.vaciarCamion
+);
 
 
 export default router;
